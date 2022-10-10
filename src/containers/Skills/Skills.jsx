887b2dc1ce89ee59fe586dc2b4430a0d571c6fb0
shivam chawla <shivamchawla.29june@gmail.com>
2022-10-10 14:45:11 -0700

    Reverting ID changes

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -41,7 +41,7 @@ const Skills = () => {
               whileInView={{ opacity: [0, 1] }}
               transition={{ duration: 0.5 }}
               className="app__skills-item app__flex"
-              key={skill._id}
+              key={skill.name}
             >
               <div
                 className="app__flex"
@@ -56,7 +56,7 @@ const Skills = () => {
         </motion.div>
         <motion.div className="app__skills-exp">
           {experience?.map((experience) => (
-            <motion.div className="app__skills-exp-item" key={experience._id}>
+            <motion.div className="app__skills-exp-item" key={experience.year}>
               <div className="app__skills-exp-year">
                 <p className="p-text">{experience.year}</p>
               </div>
@@ -67,7 +67,7 @@ const Skills = () => {
                       whileInView={{ opacity: [0, 1] }}
                       transition={{ duration: 0.5 }}
                       className="app__skills-exp-work app__flex"
-                      key={work._key}
+                      key={work.name}
                       data-tip
                       data-for={work.name}
                     >
@@ -76,7 +76,7 @@ const Skills = () => {
                       {/* <p className="p-text">{work.desc}</p> */}
                     </motion.div>
                     <ReactTooltip
-                      id={work._key}
+                      id={work.name}
                       effect="solid"
                       arrowColor="#fff"
                       className="skills-tooltip"
